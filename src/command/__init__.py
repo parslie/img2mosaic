facb9384882867faa6c9429e654fbee432328efd
Viktor Holta <viktor.holta@outlook.com>
2025-03-29 00:00:48 +0100

    Extract average colors from image sections based on density

--- src/command/__init__.py
+++ src/command/__init__.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 import click
+import cv2 as cv
 
-from command.analyze import extract_image_paths
+from command.analyze import extract_colors, extract_image_paths, extract_image_sections
 from database import Database
 
 
@@ -16,11 +17,9 @@ def analyze(profile: str, density: int, base_path: Path, recurse: bool):
     image_paths = extract_image_paths(base_path, recurse)
     with click.progressbar(image_paths, label="Analyzing images") as bar:
         for image_path in bar:
-            pass
-            # TODO:
-            #   - Load image
-            #   - Divide image into square sections
-            #   - Get the average colors of each square section
-            #   - Associate each color with the square sections in the database
+            image = cv.imread(str(image_path), cv.IMREAD_COLOR_BGR)
+            for image_section in extract_image_sections(image):
+                colors = extract_colors(image_section.pixels, density)
+                # TODO: Associate each color with the square sections in the database
 
     database.commit()
--- src/command/analyze.py
+++ src/command/analyze.py
@@ -1,4 +1,9 @@
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Generator
+
+import cv2 as cv
+import cv2.typing as cvt
 
 from util import VALID_IMAGE_EXTS
 
@@ -14,3 +19,55 @@ def extract_image_paths(base_path: Path, recurse: bool) -> list[Path] | None:
         return image_paths
     else:
         return None
+
+
+@dataclass
+class ImageSection:
+    pixels: cvt.MatLike
+    x: int
+    y: int
+    size: int
+
+
+def extract_image_sections(image: cvt.MatLike) -> Generator[ImageSection]:
+    (height, width, _) = image.shape
+
+    if width >= height:
+        section_size = height
+        section_count = width - height + 1
+        delta_x = 1
+        delta_y = 0
+    else:
+        section_size = width
+        section_count = height - width + 1
+        delta_x = 0
+        delta_y = 1
+
+    for section_idx in range(section_count):
+        x_start = delta_x * section_idx
+        y_start = delta_y * section_idx
+        x_end = x_start + section_size
+        y_end = y_start + section_size
+        yield ImageSection(
+            pixels=image[y_start:y_end, x_start:x_end],
+            x=x_start,
+            y=y_start,
+            size=section_size,
+        )
+
+
+def extract_colors(image: cvt.MatLike, density: int) -> list[tuple[int, int, int]]:
+    colors = []
+
+    subsection_height = image.shape[0] / density
+    subsection_width = image.shape[1] / density
+    for density_y in range(density):
+        for density_x in range(density):
+            x_start = round(density_x * subsection_width)
+            y_start = round(density_y * subsection_height)
+            x_end = round((density_x + 1) * subsection_width)
+            y_end = round((density_y + 1) * subsection_height)
+            (blue, green, red, _) = cv.mean(image[y_start:y_end, x_start:x_end])
+            colors.append((round(blue), round(green), round(red)))
+
+    return colors
