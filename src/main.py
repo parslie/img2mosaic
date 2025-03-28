81a54f66ce748c63b27250ba637248845f3a93fa
Viktor Holta <viktor.holta@outlook.com>
2025-03-28 20:48:58 +0100

    Prepare remake

--- src/main.py
+++ src/main.py
@@ -1,26 +1,2 @@
-from actions.analysis import Analyze
-from actions.cache import Cache
-from actions.generation import Generate
-from arguments.parsers import get_args 
-
-
-def main():
-    args = get_args()
-    action = None
-
-    match args.action:
-        case "generate":
-            action = Generate(args)
-        case "analyze":
-            action = Analyze(args)
-        case "cache":
-            action = Cache(args)
-
-    try:
-        action.run()
-    except KeyboardInterrupt:
-        action.cancel()
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    print("Hello, World!")
